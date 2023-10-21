435296b7faba6dc71d20717e73b0f68ff76dbea9
s1280148 <s1280148aizu@gmail.com>
2023-10-21 12:38:25 +0900

    カラーの定義を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./webview-main-src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  extend: {
+    colors: {
+      course: '#1sbcbc',
+      challenge: '#777ef2',
+      bookmark: '#c6a57b',
+      accept: '#16a085',
+      reject: '#ec6941',
+    },
   },
   // safelist: [
   //   {
