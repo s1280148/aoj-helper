39c1fd7fff878df4eec4e58d98c9eae5e28870d2
s1280148 <s1280148aizu@gmail.com>
2023-11-05 16:15:37 +0900

    問題集画面のブックマークタブにダークモードを適用

--- webview-main-src/pages/BookmarkListPage.tsx
+++ webview-main-src/pages/BookmarkListPage.tsx
@@ -26,9 +26,11 @@ const BookmarkListPage: React.FC = () => {
   }, []);
   return (
     <>
-      <Box className="mt-4 border-2 rounded">
-        <h2 className="text-xl font-bold mb-2 border pl-4 py-2 bg-gray-200">ブックマークリスト</h2>
-        <p className="p-1 pl-4 my-2">ブックマークに追加した問題に挑戦</p>
+      <Box className="mt-4 border-2 rounded dark:bg-darkMode-darkest dark:border-darkMode-dark dark:text-darkMode-text">
+        <h2 className="text-xl font-bold mb-2 border pl-4 py-2 bg-gray-200 dark:bg-darkMode-dark dark:border-darkMode-dark dark:text-darkMode-text">
+          ブックマークリスト
+        </h2>
+        <p className="p-1 pl-4 my-2 dark:text-darkMode-text">ブックマークに追加した問題に挑戦</p>
       </Box>
       <Box className="mt-4">
         {bookmarkInfo &&
